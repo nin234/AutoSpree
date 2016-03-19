5bf17a7f3a7107f29ddf9a5d2bed3763880ca35e
Ninan Thomas <nin234@yahoo.com>
2016-03-19 08:22:36 -0400

    sharing initial commit

--- AutoSpree/AppDelegate.m
+++ AutoSpree/AppDelegate.m
@@ -869,6 +869,8 @@
     //[self.window addSubview:self.navViewController.view];
     [self.window setRootViewController:self.navViewController];
     [self.window makeKeyAndVisible];
+    appUtl.window = self.window;
+    appUtl.tabBarController = self.tabBarController;
     if (appUtl.purchased)
         [appUtl registerForRemoteNotifications];
     
@@ -920,18 +922,10 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
     bInBackGround = true;
     self.navViewController.navigationBar.tintColor = nil;
     return;
@@ -939,17 +933,12 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
     bInBackGround = false;
     return;
 }
 
-
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Saves changes in the application's managed object context before the application terminates.
     [self.dataSync saveContext];
 }
 
