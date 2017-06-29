e2819a055566d24068f3e4c04602722753d17eab
ninanthomas <nin234@yahoo.com>
2017-06-28 22:07:36 -0400

    bug fixes

--- AutoSpree/AppDelegate.m
+++ AutoSpree/AppDelegate.m
@@ -441,8 +441,7 @@
 	self.editItem.val1 =  sec + usec ;
 
     [dataSync editedItem:self.editItem];
-    DisplayViewController *pDisp = (DisplayViewController *)[self.navViewController topViewController];
-    [pDisp.tableView reloadData];
+    [self.navViewController popViewControllerAnimated:NO];
     
 }
 
